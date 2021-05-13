ce07a766d4a9c542e2bd61b341542ceabbcf7f50
TenzingS <thesherpatenzing@gmail.com>
2021-05-13 05:49:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(x,y) {
+    return x + y;
+}
+console.log(1,2);
+
+function subtract(x,y) {
+    return x - y;
+}
+console.log(1,2);
+
+function multiply(x,y) {
+    return x * y;
+}
+console.log(1,2);
+
+function divide(x,y) {
+    return x / y;
+}
+console.log(1,2);
+
+const x = 1;
+function increment(x){
+    return ++x;
+}
+console.log(x);
+
+const y = 10;
+function decrement(y){
+    return --y;
+}
+console.log(y);
+
+function makeInt(n) {
+    return parseInt(n, 10)
+  }
+console.log(n)
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(n);
